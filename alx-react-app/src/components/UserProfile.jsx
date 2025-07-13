fb09ee911973ef1a37964ba6991068a9203cae94
Joshua misati <misatijoshua6@gmail.com>
2025-07-13 14:52:07 +0300

    minor changes

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile (props){
     return(
         <>
         <h2>{props.name}</h2>
-        <p> {props.Age}</p>
-        <p> {props.Bio}</p>
+        <p>{props.age}</p>
+        <p>{props.bio}</p>
         </>
     )
 }
