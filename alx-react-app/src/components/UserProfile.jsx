7741a618e655604238155ee73faa824e6321b2cd
Joshua misati <misatijoshua6@gmail.com>
2025-07-13 14:56:00 +0300

    added bio:

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -3,8 +3,8 @@ function UserProfile (props){
     return(
         <>
         <h2>{props.name}</h2>
-        <p>{props.age}</p>
-        <p>{props.bio}</p>
+        <p>Age: {props.age}</p>
+        <p>Bio: {props.bio}</p>
         </>
     )
 }
