2c7ffb62a0025a26731c5945492990e972b808c9
Joshua misati <misatijoshua6@gmail.com>
2025-07-13 14:46:07 +0300

    added userprofile props to app.jsx

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function UserProfile (props){
+    return(
+        <>
+        <h2>{props.name}</h2>
+        <p> {props.age}</p>
+        <p> {props.bio}</p>
+        </>
+    )
+}
+export default UserProfile;
