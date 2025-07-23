d063622b227de8937c9d8d3fc057400205ca9d6f
Joshua misati <misatijoshua6@gmail.com>
2025-07-23 18:54:37 +0300

    added default export

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,5 +1,7 @@
-import React, { createContext} from 'react';
+
+import React, { createContext } from 'react';
 
 const UserContext = createContext();
 
 export const UserProvider = UserContext.Provider;
+export default UserContext;
