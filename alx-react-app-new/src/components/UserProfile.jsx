b4d37a76e37a1df394a961a6c805abafc497b6f6
Joshua misati <misatijoshua6@gmail.com>
2025-07-23 17:51:42 +0300

    additional style

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,7 +3,7 @@ function UserProfile (props){
     return(
         <>
         <h2 style={{font: 'caption', color:'blue'}}>{props.name}</h2>
-        <p style={{ fontFamily:'sans-serif'}}>Age: {props.age}</p>
+        <p style={{ fontFamily:'sans-serif', border: '1px solid gray', padding: '10px', margin: '10px'}}>Age: {props.age}</p>
         <p style={{fontFamily:'revert-layer', fontPalette:'revert-layer',color:'purple'}}>Bio: {props.bio}</p>
         </>
     )
