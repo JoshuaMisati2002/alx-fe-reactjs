2d75b2fd22ec2c7d3bc92e334299b810c99e6da9
Joshua misati <misatijoshua6@gmail.com>
2025-08-01 17:55:07 +0300

    1st commit

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Routes, Route,Link} from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Vite + React</h1>
+    
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+     
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
