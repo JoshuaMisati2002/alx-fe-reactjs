930299289bfe5682d39306250d8e3c70b44a7f08
Joshua misati <misatijoshua6@gmail.com>
2025-08-05 19:27:07 +0300

    hover effect

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
   return (
-    <div className="bg-gray-100 p-4 max-w-xs mx-auto my-20 rounded-lg shadow-lg text-center sm:p-4 md:p-8 md:max-w-sm">
+    <div className="bg-gray-100 p-4 max-w-xs mx-auto my-20 rounded-lg shadow-lg text-center transition-shadow duration-300 ease-in-out hover:shadow-xl sm:p-4 md:p-8 md:max-w-sm">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-24 h-24 rounded-full mx-auto sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="w-24 h-24 rounded-full mx-auto transition-transform duration-300 ease-in-out hover:scale-110 sm:w-24 sm:h-24 md:w-36 md:h-36"
       />
-      <h1 className="text-lg text-blue-800 my-4 font-semibold md:text-xl">John Doe</h1>
+      <h1 className="text-lg text-blue-800 my-4 font-semibold transition-colors duration-300 ease-in-out hover:text-blue-500 md:text-xl">John Doe</h1>
       <p className="text-sm text-gray-600 md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
