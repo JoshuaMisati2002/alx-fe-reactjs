6e6ffcce2b79ae37c40b55921674ce2b3f49ea97
Joshua misati <misatijoshua6@gmail.com>
2025-08-05 18:40:02 +0300

    installed tailwind css

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
