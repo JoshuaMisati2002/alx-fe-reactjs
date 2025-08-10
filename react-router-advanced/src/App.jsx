fc72f9efcb3025f3a77048febbf2ee9cafe29ac3
Joshua misati <misatijoshua6@gmail.com>
2025-08-10 11:23:52 +0300

    changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Link } from "react-router-dom";
 import { useState } from "react";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 
@@ -13,26 +14,25 @@ function App() {
 
   return (
     <div>
-      {/* Navigation */}
-      <nav style={{ marginBottom: "1rem" }}>
-        <Link to="/" style={{ marginRight: "1rem" }}>Home</Link>
-        <Link to="/profile" style={{ marginRight: "1rem" }}>Profile</Link>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
         <Link to="/login">Login</Link>
       </nav>
 
-      {/* Routes */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
-        
-        {/* Protected Route */}
-        <Route 
-          path="/profile" 
+        <Route
+          path="/login"
+          element={<Login setIsAuthenticated={setIsAuthenticated} />}
+        />
+        <Route
+          path="/profile"
           element={
             <ProtectedRoute isAuthenticated={isAuthenticated}>
               <Profile />
             </ProtectedRoute>
-          } 
+          }
         />
       </Routes>
     </div>
